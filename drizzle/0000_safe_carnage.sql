aab00256d0e417d5898ee082bd2d53b7311be0e7
rellityuint32 <zachari.iligan@asceoft.com>
2025-04-28 14:36:40 +0800

    fix: add drizzle migration files

---
+++ drizzle/0000_safe_carnage.sql
@@ -0,0 +1,43 @@
+CREATE TABLE `comments` (
+	`id` text PRIMARY KEY NOT NULL,
+	`content` text NOT NULL,
+	`fname` text,
+	`lname` text,
+	`email` text NOT NULL,
+	`c_id` text NOT NULL,
+	`post_id` text NOT NULL,
+	`created_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	FOREIGN KEY (`post_id`) REFERENCES `posts`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `media` (
+	`id` text PRIMARY KEY NOT NULL,
+	`path` text NOT NULL,
+	`url` text NOT NULL,
+	`created_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `media_path_unique` ON `media` (`path`);--> statement-breakpoint
+CREATE TABLE `posts` (
+	`id` text PRIMARY KEY NOT NULL,
+	`title` text NOT NULL,
+	`author` text,
+	`banner_img` text,
+	`content` text NOT NULL,
+	`likes` integer DEFAULT 0 NOT NULL,
+	`created_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` text PRIMARY KEY NOT NULL,
+	`email` text NOT NULL,
+	`name` text NOT NULL,
+	`password_hash` text NOT NULL,
+	`created_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
